49d8ccd8080514762d4b9536623a00e770a1501a
Mohammed- Hamim <mohammedhamim0158@gmail.com>
2025-12-10 23:42:04 +0600

    installed firebase, created authContext and set up AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
- 
+
 import { RouterProvider } from 'react-router'
 import { router } from './routes/Router.jsx'
+import AuthProvider from './context/authProvider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
